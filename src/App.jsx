0d731a18ef7aa5428980ab98041340c1058b96b5
faizitguy <faiz.itguy@gmail.com>
2024-03-29 17:31:01 +0530

    added necessary package

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable */
+// Code to be ignored by ESLint
+/* eslint-enable */
+
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import Work from "./Components/Work";
-import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
