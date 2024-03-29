42e60ea05fd2452b0bd5e44de33574d0236307bc
faizitguy <faiz.itguy@gmail.com>
2024-03-29 17:36:27 +0530

    added necessary package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+/* eslint-disable */
 import "./App.css";
 import Home from "./Components/Home";
-import About from "./Components/About";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* <About />  */}
       <Footer />
     </div>
   );
